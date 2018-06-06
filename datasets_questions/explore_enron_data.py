c09975040bce9e5d96949b0e3fad1d64974f2abe
Benny Teo <32384531+whiteo83@users.noreply.github.com>
2018-06-06 15:12:20 +0800

    explore enrondata

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -16,7 +16,47 @@
 """
 
 import pickle
+import pprint
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
+
+### General Review of data####
+print len(enron_data)
+
+#for key in enron_data:
+#    print key
+    
+#pprint.pprint(enron_data)
+    
+
+###Counting number of Persons of interest identified in the dataset###
+poi_num = 0
+poi_key = 0 
+for key in enron_data:    
+    poi_key += 1
+    if enron_data[key]["poi"] == True:
+#        print "{}:POI{}".format(enron_data[key],enron_data[key]["poi"])
+        poi_num += 1
+    
+print "POI Keys cycled:{}".format(poi_key)        
+print "# of POI Identified:{}".format(poi_num)
+
+###Pull feature of interest belonging to an individual###
+name = "Fastow Andrew S"
+feature_of_interest = "total_payments"
+print "{}'s {}:{}".format(name, feature_of_interest, enron_data[name.upper()][feature_of_interest])
+
+
+####Data Validation####
+
+features = 0
+feature_validated = "total_payments"
+
+for key in enron_data:
+    
+    if enron_data[key][feature_validated] != "NaN":
+        features += 1
+
+print "{} have {}".format(features, feature_validated)
 
 
