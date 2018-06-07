63b5f27b0ee93af466fbc498c224eeea957dc662
Benny Teo <32384531+whiteo83@users.noreply.github.com>
2018-06-07 14:06:36 +0800

    finance_regression

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -29,7 +29,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -37,8 +37,14 @@ test_color = "b"
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
+from sklearn import linear_model
+reg = linear_model.Ridge (alpha = .5)
+reg.fit (feature_train,target_train) 
+print reg.coef_
+print reg.intercept_ 
 
-
+prediction = reg.predict(feature_test)
+print "R sq score = {}".format(reg.score(feature_test,target_test))
 
 
 
@@ -64,6 +70,11 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+###drawing a regression line without the 1M outlier###
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b") 
+print reg.coef_
+####
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
